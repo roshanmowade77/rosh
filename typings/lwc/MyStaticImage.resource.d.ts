cb0252a56f49620c465bb54b917b466c88ab2d2d
Roshan mowade <roshanmowade77@gmail.com>
2022-09-17 21:59:14 +0530

    first commit

---
+++ typings/lwc/MyStaticImage.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/MyStaticImage" {
+    var MyStaticImage: string;
+    export default MyStaticImage;
+}
---
+++ typings/lwc/MyZipFile.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/MyZipFile" {
+    var MyZipFile: string;
+    export default MyZipFile;
+}
